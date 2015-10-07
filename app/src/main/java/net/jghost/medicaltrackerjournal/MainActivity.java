4841c8a5c6bf2d14f7c51758a929f32a69747372
Jasper Zhang <jzz111@gmail.com>
2015-10-06 23:23:41 -0400

    Tested with Genymotion and started to implement "pretty" list view using link: http://www.ezzylearning.com/tutorial/customizing-android-listview-items-with-custom-arrayadapter.

--- app/src/main/java/net/jghost/medicaltrackerjournal/MainActivity.java
+++ app/src/main/java/net/jghost/medicaltrackerjournal/MainActivity.java
@@ -1,9 +1,10 @@
 package net.jghost.medicaltrackerjournal;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
